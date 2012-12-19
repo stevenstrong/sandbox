bfbea3c4668ab6f6a5b8d0f14b110278d9c8dbd2
Aidi Stan <aidistan@live.cn>
2012-12-19 22:35:22 +0800

    Changed to a Player~

--- SandBox/SandBox/MainWindow.xaml.cs
+++ SandBox/SandBox/MainWindow.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using System.IO;
+
 namespace SandBox
 {
 	/// <summary>
@@ -17,6 +19,10 @@ namespace SandBox
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		//BgmPlayer
+		private int		bgmIndex;
+		private bool	isBgmMute;
+
 		public MainWindow()
 		{
 			this.InitializeComponent();
@@ -24,7 +30,7 @@ namespace SandBox
 			// 在此点下面插入创建对象所需的代码。
 		}
 
-		public void CloseWindow( object sender, RoutedEventArgs args )
+		public void CloseWindow(object sender, RoutedEventArgs args)
 		{
 			this.Close();
 		}
@@ -34,6 +40,50 @@ namespace SandBox
 			this.DragMove();
 		}
 
+		private void BgmInitialize(object sender, EventArgs e)
+		{
+			bgmIndex = 0;
+			isBgmMute = false;
+
+			string[] bgmFiles = Directory.GetFiles("bgm","*.mp3");
+			if (bgmFiles.Length > 0)
+			{
+				BgmPlayer.Source = new Uri(bgmFiles[bgmIndex], UriKind.RelativeOrAbsolute);
+				BgmPlayer.Play();
+			}
+		}
+
+		public void BgmChange(object sender, RoutedEventArgs args)
+		{
+			string[] bgmFiles = Directory.GetFiles("bgm", "*.mp3"); 
+			if (bgmFiles.Length > 0)
+			{
+				BgmPlayer.Source = new Uri(bgmFiles[(++bgmIndex) % bgmFiles.Length], UriKind.RelativeOrAbsolute);
+				BgmPlayer.Play();
+			}
+		}
+
+		private void Button_Mute_MouseEnter(object sender, MouseEventArgs e) { Button_Mute_Clicked.Visibility = Visibility.Visible; }
+
+		private void Button_Mute_MouseLeave(object sender, MouseEventArgs e) { Button_Mute_Clicked.Visibility = Visibility.Hidden; }
+
+		public void Button_Mute_Click(object sender, RoutedEventArgs args)
+		{
+			if (isBgmMute)
+			{
+				BgmPlayer.Play();
+				isBgmMute = false;
+				Button_Mute_Forbid_Idle.Visibility = Visibility.Hidden;
+				Button_Mute_Forbid_Clicked.Visibility = Visibility.Hidden; 
+			}
+			else
+			{
+				BgmPlayer.Pause();
+				isBgmMute = true;
+				Button_Mute_Forbid_Idle.Visibility = Visibility.Visible;
+				Button_Mute_Forbid_Clicked.Visibility = Visibility.Visible; 
+			}
+		}
 	}
 	
 }
